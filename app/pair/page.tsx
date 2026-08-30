7f047f86bbc7aab9b8b446b6ff5874832b52021c
xAc30x <thbbrgwg5y@privaterelay.appleid.com>
2026-08-30 07:01:05 -0400

    Add mobile pairing page

---
+++ app/pair/page.tsx
@@ -0,0 +1,9 @@
+import { PairClient } from "./pair-client";
+
+export const metadata = {
+  title: "Pair iPhone · TableRead",
+};
+
+export default function PairPage() {
+  return <PairClient />;
+}
